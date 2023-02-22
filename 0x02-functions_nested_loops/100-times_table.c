c97fca9d0b6e766c6393b26be4e3f99f844aa61b
Empirestar247 <ejimofor81@gmail.com>
2023-02-22 10:08:30 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,20 @@
+include"main.h"
+
+/**
+print_times_table the ntimes table
+*
+*Return: void
+*/
+
+void print_times_table(int n)
+{
+        int i, j, k;
+        if (n >= 0; && n <= 15)
+        {
+                 for (j = 0; j <= n; j++)
+                 {
+                         k = i * j;
+                         if (i == 0 && j == 0)
+                                  _putchar('0');
+                          else if (k == 0)
+
