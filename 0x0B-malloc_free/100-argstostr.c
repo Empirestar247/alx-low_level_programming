dd61794bc6f90c6ce00a8f3ef604642d3fda2354
Empirestar247 <ejimofor81@gmail.com>
2023-03-13 12:23:27 -0700

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,67 +1,45 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * argstostr - concatenates all the arguments of the program
- * @ac: argument count
- * @av: argument vector
+ * argstostr - Concatenates all the arguments of a program
  *
- * Return: pointer to the new string, or NULL if it fails
- * Each argument should be followed by a \n in the new string
+ * @ac: The number of arguments
+ * @av: An array of strings containing the arguments
+ *
+ * Return: A pointer to the concatenated string, or NULL on failure
  */
 char *argstostr(int ac, char **av)
 {
-        char *str;
-        int i, j, len = 0, index = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-        if (ac == 0 || av == NULL)
-                return (NULL);
+	int total_length = 0;
+	for (int i = 0; i < ac; i++)
+	{
+		char *arg = av[i];
+		if (arg == NULL)
+			continue;
+		for (int j = 0; arg[j] != '\0'; j++)
+			total_length++;
+		total_length++;
+	}
 
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                        len++;
-                len++;
-        }
+	char *str = malloc(sizeof(char) * (total_length + 1));
+	if (str == NULL)
+		return (NULL);
 
-        str = malloc(sizeof(char) * (len + 1));
-        if (str == NULL)
-                return (NULL);
+	int pos = 0;
+	for (int i = 0; i < ac; i++)
+	{
+		char *arg = av[i];
+		if (arg == NULL)
+			continue;
+		for (int j = 0; arg[j] != '\0'; j++)
+			str[pos++] = arg[j];
+		str[pos++] = '\n';
+	}
+	str[pos] = '\0';
 
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                {
-                        str[index] = av[i][j];
-                        index++;
-                }
-                str[index] = '\n';
-                index++;
-        }
-
-        str[index] = '\0';
-        return (str);
-}
-
-int main(int argc, char **argv)
-{
-        char *concatenated_args;
-
-        if (argc < 2)
-        {
-                printf("Usage: %s arg1 arg2 ... argN\n", argv[0]);
-                return (1);
-        }
-
-        concatenated_args = argstostr(argc - 1, argv + 1);
-        if (concatenated_args == NULL)
-        {
-                printf("Error: failed to concatenate arguments\n");
-                return (1);
-        }
-
-        printf("%s", concatenated_args);
-        free(concatenated_args);
-        return (0);
+	return (str);
 }
 
