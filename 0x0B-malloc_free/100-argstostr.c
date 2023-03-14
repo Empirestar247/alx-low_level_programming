f38f2b391cdb5bf7b4dbfe040b43a49e6fde7d8c
Empirestar247 <ejimofor81@gmail.com>
2023-03-14 06:28:42 -0700

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,48 @@
-#include "main.h" 
- #include <stdlib.h> 
- /** 
-   *argstostr - concatenates all arguments of the program. 
-   *@ac: argument count. 
-   *@av: pointer to array of size ac. 
-   *Return: NULL if ac == 0 or av == null, Pointer to new string. 
-   *NULL on fail. 
-   */ 
- char *argstostr(int ac, char **av) 
- { 
-         int i, j, k, size; 
-         char *arg; 
-  
-         size = 0; 
-         k = 0; 
-         if (ac == 0 || av == NULL) 
-                 return (NULL); 
-         i = 0; 
-         while (i < ac) 
-         { 
-                 j = 0; 
-                 while (av[i][j]) 
-                 { 
-                         size++; 
-                         j++; 
-                 } 
-                 size++; 
-                 i++; 
-         } 
-         arg = malloc((sizeof(char) * size) + 1); 
-         if (arg == NULL) 
-                 return (NULL); 
-         i = 0; 
-         while (i < ac) 
-         { 
-                 j = 0; 
-                 while (av[i][j]) 
-                 { 
-                         arg[k] = av[i][j]; 
-                         j++; 
-                         k++; 
-                 } 
-                 arg[k] = '\n'; 
-                 k++; 
-                 i++; 
-         } 
-         arg[k] = '\0'; 
-         return (arg); 
- }
+#include <main.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - concatenates all the arguments of a program
+ * @ac: the number of arguments
+ * @av: an array of strings representing the arguments
+ *
+ * Return: a pointer to a new string that contains the concatenated arguments
+ *         with each argument followed by a newline character, or NULL if ac is
+ *         0 or av is NULL or if memory allocation fails.
+ */
+char *argstostr(int ac, char **av)
+{
+    int i, j, len, total_len;
+    char *str;
+
+    if (ac == 0 || av == NULL)
+        return (NULL);
+
+    /* Compute the total length of the concatenated string */
+    for (i = 0, total_len = 0; i < ac; i++)
+    {
+        for (len = 0; av[i][len]; len++)
+            total_len++;
+
+        total_len++; /* Add 1 for the \n character */
+    }
+
+    /* Allocate memory for the concatenated string */
+    str = malloc(sizeof(char) * total_len);
+    if (str == NULL)
+        return (NULL);
+
+    /* Concatenate the arguments with \n characters */
+    for (i = 0, j = 0; i < ac; i++)
+    {
+        for (len = 0; av[i][len]; len++)
+            str[j++] = av[i][len];
+
+        str[j++] = '\n';
+    }
+
+    str[j] = '\0'; /* Add the null terminator */
+
+    return (str);
+}
+
