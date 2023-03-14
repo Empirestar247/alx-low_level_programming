d90ce0e7d64b2584e79d1a21896f6321545befd7
Empirestar247 <ejimofor81@gmail.com>
2023-03-14 07:30:13 -0700

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,29 +10,25 @@
  */
 char *argstostr(int ac, char **av)
 {
-        int i, j, len = 0, n = 0;
-        char *str;
-
-        if (ac == 0 || av == NULL)
-                return (NULL);
-
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                        len++;
-                len++;
-        }
-
-        str = malloc(sizeof(char) * (len + 1));
-        if (str == NULL)
-                return (NULL);
-
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                        str[n++] = av[i][j];
-                str[n++] = '\n';
-        }
+int i, j, len = 0, n = 0;
+char *str;
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+len++;
+len++;
+}
+str = malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+str[n++] = av[i][j];
+str[n++] = '\n';
+}
 
         str[n] = '\0';
 
