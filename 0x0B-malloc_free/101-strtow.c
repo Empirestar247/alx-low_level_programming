eab920dbf2d8d5c2c169d1e06776bc0e9a19e0c1
Empirestar247 <ejimofor81@gmail.com>
2023-03-14 07:54:25 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,61 +1,68 @@
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+/**
+ * strtow - Split a string into words
+ * @str: The string to split
+ *
+ * Return: A pointer to an array of strings (words), or NULL if str is NULL or empty
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int i, j, num_words = 0, word_len = 0, start = 0, end = 0;
 
-char **strtow(char *str) {
-    if (str == NULL || *str == '\0') {
-        return NULL;
-    }
-    
-    // Count the number of words in the string
-    int num_words = 0;
-    char *p = str;
-    while (*p != '\0') {
-        while (isspace(*p)) {
-            p++;
-        }
-        if (*p != '\0') {
-            num_words++;
-            while (*p != '\0' && !isspace(*p)) {
-                p++;
-            }
-        }
-    }
-    
-    // Allocate memory for the array of words
-    char **words = malloc((num_words + 1) * sizeof(char *));
-    if (words == NULL) {
-        return NULL;
-    }
-    
-    // Copy each word into the array
-    int i = 0;
-    p = str;
-    while (*p != '\0') {
-        while (isspace(*p)) {
-            p++;
-        }
-        if (*p != '\0') {
-            char *word_start = p;
-            while (*p != '\0' && !isspace(*p)) {
-                p++;
-            }
-            char *word = malloc((p - word_start + 1) * sizeof(char));
-            if (word == NULL) {
-                // Free memory for previously allocated words
-                for (int j = 0; j < i; j++) {
-                    free(words[j]);
-                }
-                free(words);
-                return NULL;
-            }
-            strncpy(word, word_start, p - word_start);
-            word[p - word_start] = '\0';
-            words[i++] = word;
-        }
-    }
-    words[i] = NULL;
-    
-    return words;
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	while (*(str + end))
+	{
+		if (*(str + start) == ' ')
+			start++;
+		else
+		{
+			end = start;
+			while (*(str + end) != ' ' && *(str + end))
+			{
+				word_len++;
+				end++;
+			}
+			num_words++;
+			start = end;
+			word_len = 0;
+		}
+	}
+
+	words = malloc((num_words + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+
+	for (i = 0, start = 0, end = 0; i < num_words; i++)
+	{
+		while (*(str + start) == ' ')
+			start++;
+
+		end = start;
+		while (*(str + end) != ' ' && *(str + end))
+		{
+			word_len++;
+			end++;
+		}
+
+		*(words + i) = malloc((word_len + 1) * sizeof(char));
+		if (*(words + i) == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(*(words + j));
+			free(words);
+			return (NULL);
+		}
+
+		for (j = 0; j < word_len; j++)
+			*(*(words + i) + j) = *(str + start + j);
+
+		*(*(words + i) + j) = '\0';
+		start = end;
+		word_len = 0;
+	}
+
+	*(words + i) = NULL;
+	return (words);
 }
-
