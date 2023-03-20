a979381018024c74c720d4684369d619e9d59c4f
Empirestar247 <ejimofor81@gmail.com>
2023-03-20 16:53:09 -0700

    Task 0

---
+++ x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - dog credentials
+*
+*@name: name of dog
+*@age: age of dog
+*owner: owner of dog
+*
+*return 0 success
+*/
+struct dog
+{char *name;
+float age;
+char *owner;
+}; 
+/* Additional function prototype */
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
