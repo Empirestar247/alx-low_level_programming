a197b96e040ba18c9740d2575cffd3520f4d73f2
Empirestar247 <ejimofor81@gmail.com>
2023-03-20 18:37:21 -0700

    1-pi.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+*main - prints the name of the file it was compiled from by a new line
+*Return: 0 success
+*/
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
 
