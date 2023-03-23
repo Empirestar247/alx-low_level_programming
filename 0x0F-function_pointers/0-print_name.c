0ac9a71d5d888158a3f0307a5c82b4162649b2ae
Empirestar247 <ejimofor81@gmail.com>
2023-03-23 10:38:37 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name using a given function
+ *
+ * @name: the name to be printed
+ * @f: the function used to print the name
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    size_t len = 0;
-    while (name[len] != '\0') {
-        len++;
-    }
-    char *buffer = (char *)malloc((len + 1) * sizeof(char));
-    if (buffer == NULL) {
-        exit(EXIT_FAILURE);
-    }
-    for (size_t i = 0; i <= len; i++) {
-        buffer[i] = name[i];
-    }
-    f(buffer);
-    free(buffer);
+if (name != NULL && f != NULL)
+	f(name);
 }
-
