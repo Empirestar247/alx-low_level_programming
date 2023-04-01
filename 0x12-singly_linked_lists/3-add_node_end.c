9fe5eb4d08c511d8e9ce5883c19ecc4a7e2b60b9
Empirestar247 <ejimofor81@gmail.com>
2023-04-01 05:59:58 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list.
+ * add_node_end - Adds a new node at the end of a list_t list.
  *
- * @head: pointer to a pointer to the head of the list_t list.
- * @str: string to be included in the new node.
+ * @head: The head of the list.
+ * @str: The string to be added to the list.
  *
- * Return: Address of the new element, or NULL if it failed.
+ * Return: The address of the new element, or NULL if it failed.
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *current;
+	list_t *new_node, *last_node;
 
-	if (head == NULL || str == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
@@ -25,37 +24,20 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = _strlen(new_node->str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (new_node);
 	}
-
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-
-	current->next = new_node;
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = new_node;
+	}
 
 	return (new_node);
 }
 
-/**
- * _strlen - calculate the length of a string.
- * @str: string to be measured.
- *
- * Return: length of the string
- */
-
-unsigned int _strlen(const char *str)
-{
-	unsigned int len;
-
-	for (len = 0; str[len]; len++)
-		;
-
-	return (len);
-}
