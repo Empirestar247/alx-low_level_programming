22eff4c51b4d1ada81fc1f3eed7efb13434cc513
Empirestar247 <ejimofor81@gmail.com>
2023-03-31 20:26:34 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,54 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to a pointer t the head of the linked list
- * @str: string to be stored in the new node
+ * add_node - adds a node at the beginning of the linked list
  *
- * Return: adddress of the new element, or NULL if it failed
-*/
+ * @head: pointer to the first node of the list
+ * @str: string to add to new node in the list
+ *
+ * Return: NULL if it fails / starting addr of the list
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *str_copy;
-	size_t str_len;
 
-	if (str == NULL)
-		return (NULL);
+	size_t str_len = (str != NULL) ? _strlen(str) : 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	str_len = strlen(str);
-	str_copy = strdup(str);
-	if (str_copy == NULL)
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->str = str_copy;
+
 	new_node->len = str_len;
-	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
+}
+
+/**
+ * _strlen - calculate the length of a string.
+ * @str: string to be measured.
+ *
+ * Return: length of the string.
+ */
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
 }
