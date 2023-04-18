bfae220b6b217bd393284f1da938535dca27f861
Empirestar247 <ejimofor81@gmail.com>
2023-04-18 02:58:02 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,7 @@
  * premain - function that runs before main
  *
  */
-void __attribute__ ((constructor)) premain()
-
+void __attribute__ ((constructor))premain()
 {
 	printf("You're beat! and yet' you must allow,\n");
 	printf("l bore my house upon my back!\n");
