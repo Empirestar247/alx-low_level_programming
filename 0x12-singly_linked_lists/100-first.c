bf4c7a63b87aabdc473b38c7e658ac5cbf2e3015
Empirestar247 <ejimofor81@gmail.com>
2023-04-18 02:14:05 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * print_message - prints the message "You're beat! and yet, you must allow,\n
- * l bore my house upon my back!\n
+ * premain - prints a message before main function is executed
  *
- * Return: void
+ * This function is executed before the main function is called
+ * and it prints a message to the standard output
  */
-void print_message(void)
+void __attribute__ ((constructor)) premain()
+
 {
 	printf("You're beat! and yet' you must allow,\n");
 	printf("l bore my house upon my back!\n");
