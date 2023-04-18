17767b6cd1fad99c31aa4abba2ed8f538586e2c2
Empirestar247 <ejimofor81@gmail.com>
2023-04-18 04:03:39 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,52 @@
-#include <stdio.h>
+section .data
+    hello: db "Hello, Holberton", 0Ah
+    format: db "%s", 0
 
-int main()
-{
-	char message[] = "Hello, Holberton\n";
-	printf("%s", message);
-	return (0);
-}
+section .text
+    extern printf
+    extern exit
+
+    section .bss
+        buffer resb 32
+
+    section .text
+    global main
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    ; Copy the message to the buffer
+    mov rdi, buffer
+    mov rsi, hello
+    call copy_string
+
+    ; Print the message using printf
+    mov rdi, format
+    mov rsi, buffer
+    xor eax, eax
+    call printf
+
+    ; Exit the program
+    xor edi, edi
+    call exit
+
+copy_string:
+    push rbp
+    mov rbp, rsp
+
+    ; Copy string byte by byte
+    .loop:
+        mov al, byte [rsi]
+        mov byte [rdi], al
+        inc rsi
+        inc rdi
+        cmp byte [rsi - 1], 0
+        jne .loop
+
+    ; Return pointer to destination buffer
+    mov rax, rdi
+    mov rsp, rbp
+    pop rbp
+    ret
+
