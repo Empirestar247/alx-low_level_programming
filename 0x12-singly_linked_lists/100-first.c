3ba5634667039557439731c4b4cc2d9a0b50bdd8
Empirestar247 <ejimofor81@gmail.com>
2023-04-18 01:37:12 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,43 +1,13 @@
 #include <stdio.h>
 
 /**
- * beforeMain - Prints a message to the console before main is executed
+ * print_message - prints the message "You're beat! and yet, you must allow,\n
+ * l bore my house upon my back!\n
  *
- * Description: This function prints the text
- * "You're beat! and yet, you must allow,"
- * and "I bore my house upon my back!"
- * to the console using the printf function.
- * Each line of text is terminated with a newline character (\n).
+ * Return: void
  */
-
-/* Function to print the desired text to the console */
-void beforeMain(void)
+void print_message(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
-
-/**
- * main - Entry point of the program
- *
- * Description: This function serves as the entry
- * point of the program. It calls
- * the beforeMain function to print a message to the console,
- * and then prints the
- * string "Hello, world!" to the console
- * using the printf function. The function
- * returns 0 to indicate successful completion of the program.
- *
- * Return: 0 on successful completion of the program
- */
-
-int main(void)
-{
-	/* Call the beforeMain function to print the desired text to the console */
-	beforeMain();
-
-	/* Print a message to the console */
-	printf("Hello, world!\n");
-
-	return (0);
+	printf("You're beat! and yet' you must allow,\n");
+	printf("l bore my house upon my back!\n");
 }
