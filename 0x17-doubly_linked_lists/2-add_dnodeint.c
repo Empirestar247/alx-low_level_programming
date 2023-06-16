ae49a0413a7ea688b225dcaf74ad78ce3e4913d5
Empirestar247 <ejimofor81@gmail.com>
2023-06-16 16:11:15 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - Adds a new node at the beginning of a doubly linked list
+ * add_dnodeint - Adds a new node at the beginning
+ * of a doubly linked list
  *
  * @head: Pointer to the head of the list
  * @n: Value to be stored in the new node
