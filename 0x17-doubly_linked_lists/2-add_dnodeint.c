4ddf776a888a422abd6815e8513494a2b3dc6baf
Empirestar247 <ejimofor81@gmail.com>
2023-06-16 16:13:25 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,40 +2,35 @@
 
 /**
  * add_dnodeint - Adds a new node at the beginning
- * of a doubly linked list
  *
- * @head: Pointer to the head of the list
- * @n: Value to be stored in the new node
+ * @head: Head
  *
- * Return: Address of the new element, or NULL if it failed
+ * @n: Value
+ *
+ * Return: dlistint_t
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-    if (new_node == NULL)
-    {
-        /* Allocation failed */
-        return (NULL);
-    }
+	dlistint_t *temp, *node = malloc(sizeof(dlistint_t));
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        /* If the list is empty, make the new node the head */
-        *head = new_node;
-    }
-    else
-    {
-        /* If the list is not empty, update the links */
-        new_node->next = *head;
-        (*head)->prev = new_node;
-        *head = new_node;
-    }
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->prev = NULL;
+	node->next = NULL;
 
-    return (new_node);
+	if (*head == NULL)
+	{
+		node->next = NULL;
+		*head = node;
+		return (*head);
+	}
+	temp = *head;
+	node->next = temp;
+	temp->prev = node;
+	*head = node;
+	return (node);
 }
-
