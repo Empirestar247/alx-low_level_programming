0ab1cffcb26c35393facdff9a14ddfca352d8acb
Empirestar247 <ejimofor81@gmail.com>
2023-06-21 16:21:38 -0700

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -c *.c
-gcc -Wall -pedantic -Werror -Wextra -shared -o
+gcc -Wall -pedantic -Werror -Wextra -shared -o *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
