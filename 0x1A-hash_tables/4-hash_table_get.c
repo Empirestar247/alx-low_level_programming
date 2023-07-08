a99de7e4ba6643342d75568a89d0baaf24f8be49
Empirestar247 <ejimofor81@gmail.com>
2023-07-07 21:32:00 -0700

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,73 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * make_hash_node - creates a new hash node
- * @key: key for the node
- * @value: for the node
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: The hash table
+ * @key: The key to retrieve the value
  *
- * Return: the new node, or NULL on failure
+ * Return: The value associated with the key, or NULL if key not found
  */
-hash_node_t *make_hash_node(const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
+	unsigned long int index;
 
-	node = malloc(sizeof(hash_node_t));
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
 	if (node == NULL)
 		return (NULL);
-	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
+
+	while (strcmp(node->key, key) != 0)
+		node = node->next;
+
+	if (node == NULL)
 		return (NULL);
-	}
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (NULL);
-	}
-	node->next = NULL;
-	return (node);
+
+	return (node->value);
 }
 
-
-/**
- * hash_table_set - sets a key to a value in the hash table
- * @ht: hash table to add elemt to
- * @key: key for the data
- * @value: data to store
- *
- * Return: 1 if successful, 0 otherwise
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int index;
-	hash_node_t *hash_node, *tmp;
-	char *new_value;
-
-	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-	    key == NULL || strlen(key) == 0 || value == NULL)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-	while (tmp != NULL)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-			free(tmp->value);
-			tmp->value = new_value;
-			return (1);
-		}
-		tmp = tmp->next;
-	}
-	hash_node = make_hash_node(key, value);
-	if (hash_node == NULL)
-		return (0);
-	hash_node->next = ht->array[index];
-	ht->array[index] = hash_node;
-	return (1);
-}
