68a8cfcf3654d4efc4862bde03ec89ed3033bbef
Empirestar247 <ejimofor81@gmail.com>
2023-07-07 21:48:54 -0700

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table and its elements.
+ * @ht: A pointer to the hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+
+	hash_node_t *node, *tmp;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
+
